637fa5a1924242796f3427225ce83ccb951b2d86
Arpit Singh <arpitsinghnitd@gmail.com>
2016-02-21 11:09:56 +0530

    [Project Added] Experimenting with Visibilty of layout elements in xml

---
+++ PrgramLayout/app/src/main/java/com/example/arpit/prgramlayout/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.arpit.prgramlayout;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+           Button b1,b2;
+           TextView t1;
+           boolean s=true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        b1=(Button)findViewById(R.id.button1);
+        b2=(Button)findViewById(R.id.button2);
+        t1=(TextView)findViewById(R.id.t1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(s) {
+                    t1.setVisibility(View.VISIBLE);
+                    s=false;
+                }
+                else {
+                    t1.setVisibility(View.GONE);
+                    s=true;
+                }
+            } });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
